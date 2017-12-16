038faffcea51409cb326104ef27c3a33b4f8b813
shimingxin <604393158@qq.com>
2017-12-16 20:50:29 +0800

    直播弹幕

--- danmu/settings.py
+++ danmu/settings.py
@@ -14,7 +14,7 @@ STORAGE_ASYNC = False
 
 PARSER_CLASS = 'RegexParser'
 
-FILE_STORAGE_REPOSITORY = '/home/junjie/msg'
+FILE_STORAGE_REPOSITORY = '/Users/shimingxin/Documents/msg'
 FILE_STORAGE_DATE_FORMAT = '%Y_%m_%d'
 FILE_STORAGE_NAME_FORMAT = FILE_STORAGE_REPOSITORY + '/{name}_{date}.txt'
 
