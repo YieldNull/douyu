b70e2cb2e872ab39e8f8f14d5676901f38421938
YieldNull <yieldnull@gmail.com>
2018-01-08 01:02:09 +0800

    raw2msg

--- live/consumer.py
+++ live/consumer.py
@@ -1,24 +1,58 @@
 import json
+import queue
+
+import os
+import threading
+
+import time
+
 from common.mq import ParserConsumer, StreamProducer
-from common.parser import ParserStorage
+from common.parser import MsgCsvStorage
 from live import get_logger
 
 
 class Consumer(object):
 
-    def __init__(self, storage):
+    def __init__(self, repo):
         self.producer = StreamProducer(get_logger('StreamProducer'))
-        self.storage = storage
+
+        self.date = time.strftime('%Y_%m_%d')
+
+        self.storage = MsgCsvStorage(os.path.join(repo, self.date))
+
+        self.jobs = queue.Queue()
+
+        self.repo = repo
+
+        self.thread = threading.Thread(target=self._handler_thread)
+        self.thread.start()
 
     def on_msg(self, msg):
-        self.storage.store(msg)
+        if not self.thread.is_alive():
+            self.thread = threading.Thread(target=self._handler_thread)
+            self.thread.start()
+
+        self.jobs.put(msg)
+
         self.producer.send(StreamProducer.ROUTE_STREAM + msg['roomID'], json.dumps(msg))
+
+    def _handler_thread(self):
+        while True:
+            date = time.strftime('%Y_%m_%d')
+            if date != self.date:
+                self.date = date
+                self.storage.close()
+                self.storage = MsgCsvStorage(os.path.join(self.repo, self.date))
+
+            msg = self.jobs.get(block=True)
+
+            self.storage.store(msg)
 
 
 if __name__ == '__main__':
     import sys
 
-    consumer = Consumer(ParserStorage(sys.argv[1]))
+    consumer = Consumer(sys.argv[1])
 
     p = ParserConsumer(get_logger('ParserConsumer'), msg_handler=consumer.on_msg)
     p.start()
