809eabe19466d1c81bca41ce68694fbe3ccf0fae
YieldNull <yieldnull@gmail.com>
2018-01-25 15:11:22 +0800

    fix bugs in updating room cate

--- etl/warehouse/roomcate.py
+++ etl/warehouse/roomcate.py
@@ -6,12 +6,27 @@ from etl import settings
 def update_room():
     mongo = pymongo.MongoClient(settings.MONGO_URI)[settings.MONGO_DATABASE]
     for doc in mongo['room'].find():
-        RoomCate.insert(cate_key=doc['cid'], cate_id=doc['cid'], name=doc['cate']).upsert().execute()
+        try:
+            rc, created = RoomCate.get_or_create(cate_key=doc['cid'],
+                                                 defaults={'cate_id': doc['cid'], 'name': doc['cate']})
+            if (not created) and (rc.name != doc['cate']):
+                RoomCate.update(name=doc['cate']).where(RoomCate.cate_key == doc['cid']).execute()
 
-        Room.insert(room_key=doc['rid'], room_id=doc['rid'], name=doc['roomName'],
-                    anchor=doc['nickName']).upsert().execute()
+            r, created = Room.get_or_create(room_key=doc['rid'],
+                                            defaults={'room_id': doc['rid'], 'name': doc['roomName'],
+                                                      'anchor': doc['nickName']})
 
-        RoomCateMap.insert(room_key=doc['rid'], cate_key=doc['cid']).upsert().execute()
+            if (not created) and (r.name != doc['roomName'] or r.anchor != doc['nickName']):
+                Room.update(name=doc['roomName'], anchor=doc['nickName']).where(Room.room_key == doc['rid']).execute()
+
+            rcm, created = RoomCateMap.get_or_create(room_key=doc['rid'], defaults={'cate_key': doc['cid']})
+            if (not created) and (rcm.cate_key != int(doc['cid'])):
+                # RoomCateMap.update(cate_key=doc['cid']).where(RoomCateMap.room_key == doc['rid']).execute()
+                RoomCateMap.delete().where(RoomCateMap.room_key == doc['rid']).execute()
+                RoomCateMap.insert(room_key=doc['rid'], cate_key=doc['cid'])
+
+        except Exception as e:
+            print('%s : %s' % (str(doc['rid']), repr(e)))
 
 
 if __name__ == '__main__':
